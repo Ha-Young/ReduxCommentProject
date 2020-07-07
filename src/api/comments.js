423a343ef7103f64f8a463fc5685a17daa6340eb
hychoi <hayeong28@naver.com>
2020-07-07 21:11:44 +0900

    add: make getComments api using axios

---
+++ src/api/comments.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const URL_COMMENTS = "http://localhost:4000/comments";
+
+export const getComments = async () => {
+  const response = await axios.get(URL_COMMENTS);
+
+  return response;
+};
