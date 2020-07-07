d9425bb6fc15e588e006cd8bef1ca89574b238eb
hychoi <hayeong28@naver.com>
2020-07-07 21:08:42 +0900

    add: make  comment redux && make getComments thunk function

--- src/store/modules/comments.js
+++ src/store/modules/comments.js
@@ -1,17 +1,55 @@
-import * as commentsApi from '../../api/comments';
+import * as commentsApi from "../../api/comments";
 
+const GET_COMMENTS = "comments/GET_COMMENTS";
+const GET_COMMENTS_SUCCESS = "comments/GET_COMMENTS_SUCCESS";
+const GET_COMMENTS_ERROR = "comments/GET_COMMENTS_ERROR";
 
-export const initialState = {
-    
+const CREATE_COMMENT = "comment/CREATE_COMMENT";
+const UPDATE_COMMENT = "comment/UPDATE_COMMENT";
+const DELETE_COMMENT = "comment/DELETE_COMMENT";
+
+export const getComments = () => async (dispatch) => {
+  dispatch({ type: GET_COMMENTS }); // 요청 시작
+  try {
+    const comments = await commentsApi.getComments(); // API 호출
+    dispatch({ type: GET_COMMENTS_SUCCESS, comments }); // 성공
+  } catch (error) {
+    dispatch({ type: GET_COMMENTS_ERROR, error }); // 애러
+  }
 };
 
+export const initialState = {
+  posts: {
+    loading: false,
+    data: null,
+    error: null,
+  },
+};
 
-export default function comments ( state = initialState , action ){
-    switch (action.type){
-        
-        default : 
-            return state
-
-    }
-
+export default function comments(state = initialState, action) {
+  switch (action.type) {
+    case GET_COMMENTS:
+      return {
+        ...state,
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case GET_COMMENTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.comments,
+        error: null,
+      };
+    case GET_COMMENTS_ERROR:
+      return {
+        ...state,
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
 }
